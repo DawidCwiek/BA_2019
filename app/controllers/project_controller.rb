791e18eefcf5bc5dd21e471a534966882b1f2a22
DawidCwiek <cwiekdawid1@gmail.com>
2019-07-11 13:26:40 +0200

    project controller json views

--- app/controllers/project_controller.rb
+++ app/controllers/project_controller.rb
@@ -1,9 +1,70 @@
 class ProjectController < ApplicationController
+  before_action :set_project, only: [:show, :edit, :update, :destroy]
 
   def index
-    @projects = Post.where(user_id: current_user)
+    @projects = Project.where(user_id: current_user)
   end
 
 
+  def show
+  end
+
+
+  def new
+    @project = Project.new
+  end
+
+
+  def edit
+  end
+
+
+  def create
+    @project = Project.new(project_params)
+
+    respond_to do |format|
+      if @project.save
+        format.html { redirect_to @project, notice: 'Project was successfully created.' }
+        format.json { render :show, status: :created, location: @project }
+      else
+        format.html { render :new }
+        format.json { render json: @project.errors, status: :unprocessable_entity }
+      end
+    end
+  end
+
+
+  def update
+    respond_to do |format|
+      if @project.update(project_params)
+        format.html { redirect_to @project, notice: 'Project was successfully updated.' }
+        format.json { render :show, status: :ok, location: @project }
+      else
+        format.html { render :edit }
+        format.json { render json: @project.errors, status: :unprocessable_entity }
+      end
+    end
+  end
+
+
+  def destroy
+    @project.destroy
+    respond_to do |format|
+      format.html { redirect_to posts_url, notice: 'Post was successfully destroyed.' }
+      format.json { head :no_content }
+    end
+  end
+
+
+  private
+    # Use callbacks to share common setup or constraints between actions.
+    def set_project
+      @project = Project.find(params[:id])
+    end
+
+    # Never trust parameters from the scary internet, only allow the white list through.
+    def project_params
+      params.require(:project).permit(:title, :desc, :key, :user_id)
+    end
 
 end
