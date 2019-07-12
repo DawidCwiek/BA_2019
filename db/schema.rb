69bfd4c21e1ec7009feda93f1646ed9e9d6fb97e
kucziii <kucziii@MacBook-Pro.local>
2019-07-12 11:31:44 +0200

    Styled forgot password page

--- db/schema.rb
+++ db/schema.rb
@@ -15,12 +15,6 @@ ActiveRecord::Schema.define(version: 2019_07_10_094408) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
-  create_table "posts", force: :cascade do |t|
-    t.string "title"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-  end
-
   create_table "projects", force: :cascade do |t|
     t.string "title"
     t.text "desc"
