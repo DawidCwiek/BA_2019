8e76d78780a9b19c824fe2c0ceb3ab8ac7a6dd1a
DawidCwiek <44263943+DawidCwiek@users.noreply.github.com>
2019-07-12 12:00:35 +0200

    Update routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -4,10 +4,6 @@ Rails.application.routes.draw do
 
   resources :task
   resources :manage_io, only: ['index']
-<<<<<<< HEAD
-
-=======
->>>>>>> b5d731e494b284e8c201c7f07931c0cdef79379c
   root to: 'manage_io#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
