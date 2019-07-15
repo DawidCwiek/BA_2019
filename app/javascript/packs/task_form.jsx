060aead66eea6a61cce070e8caaa71e539a02dfe
timedar <timedar@gmail.com>
2019-07-15 09:06:04 +0200

    Modal in task_form

--- app/javascript/packs/task_form.jsx
+++ app/javascript/packs/task_form.jsx
@@ -1,40 +1,74 @@
-import React from 'react'
-import ReactDOM from 'react-dom'
-import PropTypes from 'prop-types'
+import React from "react";
+import ReactDOM from "react-dom";
+import PropTypes from "prop-types";
 
-const Hello = props => (
-    <div class="modal" tabindex="-1" role="dialog">
-    <div class="modal-dialog" role="document">
-      <div class="modal-content">
-        <div class="modal-header">
-          <h5 class="modal-title">Modal title</h5>
-          <button type="button" class="close" data-dismiss="modal" aria-label="Close">
-            <span aria-hidden="true">&times;</span>
-          </button>
-        </div>
-        <div class="modal-body">
-          <p>Modal body text goes here.</p>
-        </div>
-        <div class="modal-footer">
-          <button type="button" class="btn btn-primary">Save changes</button>
-          <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
-        </div>
+import { Button, Modal, ModalHeader, ModalBody, ModalFooter, Form, FormGroup, Label, Input, FormText  } from "reactstrap";
+
+class ModalExample extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      modal: false
+
+    };
+
+    this.toggle = this.toggle.bind(this);
+  }
+
+  toggle() {
+    this.setState(prevState => ({
+      modal: !prevState.modal
+    }));
+  }
+
+  render() {
+    return (
+      <div>
+        <Button color="danger" onClick={this.toggle}>
+         Create Task{this.props.buttonLabel}
+          </Button>
+        <Modal
+          isOpen={this.state.modal}
+          modalTransition={{ timeout: 700 }}
+          backdropTransition={{ timeout: 1300 }}
+          toggle={this.toggle}
+          className={this.props.className}
+        >
+          <ModalHeader toggle={this.toggle}>Create Task</ModalHeader>
+          <ModalBody>
+            <Form>
+              <FormGroup>
+                <Label for="exampleEmail">Email</Label>
+                <Input type="email" name="email" id="exampleEmail" placeholder="write email" />
+
+            </FormGroup>
+            <FormGroup>
+                 <Label for="exampleText">Description</Label>
+                 <Input type="textarea" name="text" id="task description" placeholder="write disc" />
+            </FormGroup>
+            <FormGroup>
+                <Label for="exampleEmail">Assign to:</Label>
+                <Input type="email" name="email" id="exampleEmail" placeholder="person assign" />
+            </FormGroup>
+            </Form>
+            <Button color="primary" onClick={this.toggle}>
+              Confirm
+            </Button>
+            <Button color="secondary" onClick={this.toggle}>
+              Cancel
+            </Button>
+          </ModalBody>
+        </Modal>
       </div>
-    </div>
-  </div>
-)
-
-Hello.defaultProps = {
-  name: 'David'
+    );
+  }
 }
 
-Hello.propTypes = {
-  name: PropTypes.string
-}
+export default ModalExample;
 
-document.addEventListener('DOMContentLoaded', () => {
+document.addEventListener("DOMContentLoaded", () => {
   ReactDOM.render(
-    <Hello name="React" />,
-    document.body.appendChild(document.createElement('div')),
-  )
-})
+    <ModalExample/>,
+    document.body.appendChild(document.createElement("div"))
+  );
+});
