61b0ea86e4a98fef3a2e8ea253524647705733f2
timedar <timedar@gmail.com>
2019-07-15 16:06:47 +0200

    List of users in modal

--- app/javascript/packs/task_form.jsx
+++ app/javascript/packs/task_form.jsx
@@ -2,25 +2,34 @@ import React from "react";
 import ReactDOM from "react-dom";
 import PropTypes from "prop-types";
 
-import { Button, Modal, ModalHeader, ModalBody, ModalFooter, Form, FormGroup, Label, Input, FormText  } from "reactstrap";
+import { Button, Modal, ModalHeader, ModalBody, ModalFooter, Form, FormGroup, Label, Input, 
+  FormText,ButtonDropdown, DropdownToggle, DropdownMenu, DropdownItem  } from "reactstrap";
 
 class ModalExample extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      modal: false
-
+      modal: false,
+      dropdownOpen: false
     };
 
     this.toggle = this.toggle.bind(this);
+    this.toggle_drop = this.toggle_drop.bind(this);
   }
 
   toggle() {
     this.setState(prevState => ({
-      modal: !prevState.modal
+      modal: !prevState.modal,
     }));
   }
 
+  toggle_drop() {
+    this.setState(prevState => ({
+      dropdownOpen: !prevState.dropdownOpen
+    }));
+  }
+
+  
   render() {
     return (
       <div>
@@ -32,23 +41,37 @@ class ModalExample extends React.Component {
           modalTransition={{ timeout: 700 }}
           backdropTransition={{ timeout: 1300 }}
           toggle={this.toggle}
+          toggle_drop={this.toggle_drop}
           className={this.props.className}
         >
           <ModalHeader toggle={this.toggle}>Create Task</ModalHeader>
           <ModalBody>
             <Form>
               <FormGroup>
-                <Label for="exampleEmail">Email</Label>
-                <Input type="email" name="email" id="exampleEmail" placeholder="write email" />
+                <Label for="taskTitle">Title</Label>
+                <Input type="text" minLength="3" maxLength="30" name="text" id="taskTitle" placeholder="write task title" />
 
             </FormGroup>
             <FormGroup>
                  <Label for="exampleText">Description</Label>
-                 <Input type="textarea" name="text" id="task description" placeholder="write disc" />
+                 <Input type="textarea" maxLength="160" name="text" id="task description" placeholder="write disc" />
             </FormGroup>
             <FormGroup>
-                <Label for="exampleEmail">Assign to:</Label>
-                <Input type="email" name="email" id="exampleEmail" placeholder="person assign" />
+            <Label for="exampleText">Users to assign</Label>
+            </FormGroup>
+            <FormGroup>
+              <ButtonDropdown isOpen={this.state.dropdownOpen} toggle_drop={this.toggle_drop}>
+                <DropdownToggle caret>
+                  List of Users
+                </DropdownToggle>
+                <DropdownMenu>
+                  <DropdownItem header>Header</DropdownItem>
+                  <DropdownItem disabled>Action</DropdownItem>
+                  <DropdownItem>Another Action</DropdownItem>
+                  <DropdownItem divider />
+                  <DropdownItem>Another Action</DropdownItem>
+                </DropdownMenu>
+              </ButtonDropdown>
             </FormGroup>
             </Form>
             <Button color="primary" onClick={this.toggle}>
