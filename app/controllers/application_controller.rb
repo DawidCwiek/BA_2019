a22485b6d8a9fd48e900c62fc14f78daf0dc42cd
kucziii <kucziii@MacBook-Pro.local>
2019-07-16 14:21:32 +0200

    Done responsive sidenav layout and styling

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,10 +2,15 @@
 
 class ApplicationController < ActionController::Base
   before_action :configure_permitted_parameters, if: :devise_controller?
+  # layout :dupa
 
   protected
 
   def configure_permitted_parameters
     devise_parameter_sanitizer.permit(:sign_up, keys: %i[full_name email password])
   end
+
+  # def dupa
+  #   current_user ? 'application' : 'login'
+  # end
 end
