25e6e1f1a280b135b122b836d738e0ea8312a261
kucziii <kucziii@MacBook-Pro.local>
2019-07-16 09:27:34 +0200

    more changes to menu

--- app/javascript/packs/SideNav.jsx
+++ app/javascript/packs/SideNav.jsx
@@ -1,16 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-class SideNav extends React.Component {
+export class SideNav extends React.Component {
   render() {
     return (
       <div className="wrapper">
-      <nav id="sidebar">
-        <div className="sidebar-header">
+        <nav id="sidebar">
+          <div className="sidebar-header">
             <h1>Manage.io</h1>
-        </div>
+          </div>
 
-        <ul className="list-unstyled components">
+          <ul className="list-unstyled components">
             <li>
                 <a href="#">Kanban</a>
             </li>
@@ -18,14 +18,18 @@ class SideNav extends React.Component {
                 <a href="#">Backlog</a>
             </li>
             <li>
-              <a href="#">Create task</a>
+              <a href="#" class="border_bottom">Create task</a>
             </li>
             <li>
               <a href="#">Logout</a>
             </li>
-        </ul>
-      </nav>
-    </div>
+          </ul>
+        </nav>
+
+        <div class="content">
+          <%= yield %>
+        </div>
+      </div>
     )
   }
 };
