6faf4a9c8c34a2a45f76d5839480ec9c25a162ef
aroon0808 <damiangronowski23@gmail.com>
2019-07-18 10:46:32 +0200

    [add] admin attribute

--- db/schema.rb
+++ db/schema.rb
@@ -10,15 +10,21 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_07_16_074046) do
+ActiveRecord::Schema.define(version: 2019_07_18_084002) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
-  create_table "posts", force: :cascade do |t|
-    t.string "title"
+  create_table "admins", force: :cascade do |t|
+    t.string "email", default: "", null: false
+    t.string "encrypted_password", default: "", null: false
+    t.string "reset_password_token"
+    t.datetime "reset_password_sent_at"
+    t.datetime "remember_created_at"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.index ["email"], name: "index_admins_on_email", unique: true
+    t.index ["reset_password_token"], name: "index_admins_on_reset_password_token", unique: true
   end
 
   create_table "projects", force: :cascade do |t|
@@ -34,6 +40,7 @@ ActiveRecord::Schema.define(version: 2019_07_16_074046) do
   create_table "projects_users", force: :cascade do |t|
     t.bigint "user_id"
     t.bigint "project_id"
+    t.boolean "isActive", default: true
     t.index ["project_id"], name: "index_projects_users_on_project_id"
     t.index ["user_id"], name: "index_projects_users_on_user_id"
   end
@@ -65,6 +72,8 @@ ActiveRecord::Schema.define(version: 2019_07_16_074046) do
     t.datetime "locked_at"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.boolean "admin"
+    t.boolean "super_admin"
     t.index ["confirmation_token"], name: "index_users_on_confirmation_token", unique: true
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
