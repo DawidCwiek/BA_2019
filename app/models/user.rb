34de3689406dde3bce823b671e33262251370e4b
kucziii <kucziii@MacBook-Pro.local>
2019-07-19 15:54:05 +0200

    uncomment

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,7 +4,7 @@ class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :validatable#, :confirmable
+         :recoverable, :rememberable, :validatable, :confirmable
 
   has_many :task
   has_and_belongs_to_many :projects
