c8949a5f40ec6b8dcf7c60d24166df63051ff2e9
DawidCwiek <cwiekdawid1@gmail.com>
2019-07-22 14:19:04 +0200

    add manege_io project

--- app/controllers/manage_io_controller.rb
+++ app/controllers/manage_io_controller.rb
@@ -5,4 +5,12 @@ class ManageIoController < ApplicationController
     @projects = Project.all
     @task = Task.all
   end
+
+  def project
+    @project = Project.find(params[:id])
+
+    if @project.users.where(id: current_user.id).empty?
+      return redirect_to manage_io_index_path
+    end
+  end
 end
