e4c8c1ec95fd40845c975916abc131b1038b477a
aroon0808 <damiangronowski23@gmail.com>
2019-07-22 11:25:55 +0200

    [add] react component to html

--- app/javascript/packs/users_list.jsx
+++ app/javascript/packs/users_list.jsx
@@ -2,97 +2,98 @@ import React from "react";
 import ReactDOM from "react-dom";
 import axios from "axios";
 
-
-import { Button, Modal, ModalHeader, ModalBody, Form, FormGroup, Label, Input
-  ,ButtonDropdown, DropdownToggle, DropdownMenu, DropdownItem  } from "reactstrap";
-
-
-
-
 class UsersList extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      users_data: [],
+      users_data: []
     };
-
-
   }
- 
-    // handleSubmit = e => {
-    //         this.setState({
-    //         data: {
-    //           title: "",
-    //           desc: "",
-    //           user_id: "",
-    //           project_id: 1
-    //         }
-            
-    //       });
-         
-    //       if(this.handleValidation()){
-    //       e.preventDefault();
-          
-    //       this.setState(prevState => ({
-    //         modal: !prevState.modal,
-    //       }));
 
-    //   axios
-    //     .post(
-    //       "task.json",
-    //       { task: this.state.data },
-    //     {
-    //         headers: {
-    //           "X-CSRF-Token": document.querySelector('meta[name="csrf-token"]')
-    //             .content
-    //         }
-    //       }
-    //     )
-    //     .catch(e => {
-	// 				if(e.response.data.errors["key"] !== "undefined"){
-	// 					this.setState({errors: {key: "The key must be unique"}});
-	// 				}
-	// 			})
-    //   }
-    // }
+  // handleSubmit = e => {
+  //         this.setState({
+  //         data: {
+  //           title: "",
+  //           desc: "",
+  //           user_id: "",
+  //           project_id: 1
+  //         }
 
-    userDataTaker = () =>{
- 
-       axios
-        .get(
-          "users_list.json",
-        {
-            headers: {
-              "X-CSRF-Token": document.querySelector('meta[name="csrf-token"]')
-                .content
-            }
-          })
-          .then(response => {
-          this.setState({ users_data: response.data.data });
-        });
-      }
+  //       });
 
-  showUsers=()=>{
-    return this.state.users_data.map(userData => 
-   <li key={userData.id}> {userData.full_name}
-        </li>)
-  }
+  //       if(this.handleValidation()){
+  //       e.preventDefault();
+
+  //       this.setState(prevState => ({
+  //         modal: !prevState.modal,
+  //       }));
+
+  //   axios
+  //     .post(
+  //       "task.json",
+  //       { task: this.state.data },
+  //     {
+  //         headers: {
+  //           "X-CSRF-Token": document.querySelector('meta[name="csrf-token"]')
+  //             .content
+  //         }
+  //       }
+  //     )
+  //     .catch(e => {
+  // 				if(e.response.data.errors["key"] !== "undefined"){
+  // 					this.setState({errors: {key: "The key must be unique"}});
+  // 				}
+  // 			})
+  //   }
+  // }
+
+  userDataTaker = () => {
+    axios
+      .get("users_list.json", {
+        headers: {
+          "X-CSRF-Token": document.querySelector('meta[name="csrf-token"]')
+            .content
+        }
+      })
+      .then(response => {
+        this.setState({ users_data: response.data.data });
+      });
+  };
+
+  showUsers = () => {
+    return this.state.users_data.map(userData => (
+      <li key={userData.id}> {userData.full_name}</li>
+    ));
+  };
 
   componentDidMount() {
     this.userDataTaker();
   }
 
-
-
-
   render() {
     return (
-      <div>
-         <div className="Id">{this.state.users_data.map(userData => <div key={userData.id}> {userData.id}</div>)}</div>
-         <div className="Full_Name">{this.state.users_data.map(userData => <div key={userData.id}> {userData.full_name}</div>)}</div>
-         <div className="Email">{this.state.users_data.map(userData => <div key={userData.id}> {userData.email}</div>)}</div>
-         <div className="IsAdmin">{this.state.users_data.map(userData => <div key={userData.id}> {userData.admin}</div>)}</div>
-      </div>
+      <tr>
+        <th className="Id">
+          {this.state.users_data.map(userData => (
+            <div key={userData.id}> {userData.id}</div>
+          ))}
+        </th>
+        <td className="Full_Name">
+          {this.state.users_data.map(userData => (
+            <div key={userData.id}> {userData.full_name}</div>
+          ))}
+        </td>
+        <td className="Email">
+          {this.state.users_data.map(userData => (
+            <div key={userData.id}> {userData.email}</div>
+          ))}
+        </td>
+        <td className="IsAdmin">
+          {this.state.users_data.map(userData => (
+            <div key={userData.id}> {userData.admin}</div>
+          ))}
+        </td>
+      </tr>
     );
   }
 }
@@ -100,9 +101,6 @@ class UsersList extends React.Component {
 export default UsersList;
 
 document.addEventListener("DOMContentLoaded", () => {
-  ReactDOM.render(
-    <UsersList />,
-    document.getElementById('users_list'))
-  
+  ReactDOM.render(<UsersList />, document.getElementById("tbody"));
 });
 // json and data saving lerned from this site: https://medium.com/@everdimension/how-to-handle-forms-with-just-react-ac066c48bd4f
