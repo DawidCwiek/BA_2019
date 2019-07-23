17ed03348d37cff27b8d2c58e11ca0d5e08769d7
aroon0808 <damiangronowski23@gmail.com>
2019-07-23 14:44:54 +0200

    code refactoring

--- app/controllers/administrators_controller.rb
+++ app/controllers/administrators_controller.rb
@@ -1,17 +1,11 @@
 class AdministratorsController < ApplicationController
   before_action :authenticate_user!
-  before_action :set_user, only: %i[add_admin]
 
   def index; end
 
   def add_admin
+    @user = User.find(params[:id])
     @user.update(admin: true)
     head :no_content
-  end
-
-  private
-
-  def set_user
-    @user = User.find(params[:id])
   end
 end
