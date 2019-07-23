10793a335b1be19d831ac60373927ae129f950f9
timedar <timedar@gmail.com>
2019-07-23 16:45:06 +0200

    last changes of scheme dnd

--- app/javascript/packs/dnd_columns.jsx
+++ app/javascript/packs/dnd_columns.jsx
@@ -2,101 +2,136 @@ import React, { useState, useEffect } from "react";
 import ReactDOM from "react-dom";
 import React, { useRef } from "react";
 import { useDrag, useDrop } from "react-dnd";
-import { DndProvider } from "react-dnd";
-import HTML5Backend from "react-dnd-html5-backend";
-import { findIndex, moveElement } from "./collection_helpers";
 
-class Columns extends React.Component {
-    constructor(props) {
-      super(props);
-      this.state = {
-        columnsData: []
-      };
-
-const [columns, updateColumns] = useState(columnsData);
-
-columnsDataTaker = () =>{
-
-axios
-    .get(
-    "projects.json",
-    {
-        headers: {
-        "X-CSRF-Token": document.querySelector('meta[name="csrf-token"]')
-            .content
-        }
-    })
-    .then(response => {
-    this.setState({ columnsData: response.data.data });
-    });
-}
-
-componentDidMount() {
-    this.columnsDataTaker();
-}
-
-const moveColumn = (id, columnTitle, targetId) => {
-updateColumns(columns => {
-    console.log(id, targetId);
-
-    const columnIndex = findIndex(columns, columns => columns.id === id);
-
-    const targetIndex = targetId
-    ? findIndex(columns, column => column.id === targetId)
-    : columns.length;
-
-    const newList = moveElement(posts, columnIndex, targetIndex)
-
-    return newList.map(column => {
-    if (column.id === id) {
-        return { ...column};
-    } else {
-        return column;
+const ColumnArea = ({ columnName, id, moveColumn }) => {
+  const [, drop] = useDrop({
+    accept: itemType,
+    drop: (item, monitor) => {
+      if(!monitor.didDrop()) {
+        moveColumn(item.id, columnName);
+      }
     }
-    });
   });
+  return(
+    <div ref={drop}></div>
+  );
+}
+
+const Column = ({ id, columnName, moveColumn }) => {
+  const ref = useRef();
+
+  const [, drag] = useDrag({
+    item: { id: id, columnName, type: itemType }
+  });
+
+  const [, drop] = useDrop({
+    accept: itemType,
+    drop: item => {
+      moveColumn(item.id, columnName, id)
+    }
+  })
+
+  drag(ref);
+  drop(ref);
+  return(
+    <table  moveColumn={moveColumn} key={Column.id} ref={ref}>
+      <tr>{columnName}</tr>
+      <td></td>
+    </table>
+  );
 };
 
-const Column = ({ id, columnTitle, moveColumn }) => {
-    const ref = useRef();
-  
-    const [, drag] = useDrag({
-      item: { id: id, type: itemType }
+export default ColumnArea;
+
+const itemType = "COLUMN";
+/////////////////////////////////////////
+const Posts = () => {
+  const [posts, updatePost] = useState(initialData);
+
+  const moveColumn = (id, columnName, targetId) => {
+    updatePost(posts => {
+      console.log(id, targetId);
+
+      const postIndex = findIndex(posts, post => Column.id === id);
+
+      const targetIndex = targetId
+        ? findIndex(posts, post => Column.id === targetId)
+        : posts.length;
+
+      const newList = moveElement(posts, postIndex, targetIndex)
+
+      return newList.map(post => {
+        if (post.id === id) {
+          return { ...post, status: columnName };
+        } else {
+          return post;
+        }
+      });
     });
-  
-    const [, drop] = useDrop({
-      accept: itemType,
-      drop: item => {
-        moveColumn(item.id, columnTitle, id)
-      }
-    })
-  
-    drag(ref);
-    drop(ref);
-  
-    return (
-        <table className="col-4">
-          <th>{columnTitle}</th>
-          <td className="list-group" style={{ paddingBottom: 100 }} ref={drop}>
-            {posts.map(column => (
-              <Post
-                key={column.id}
-                id={column.id}
-                columnTitle={columnTitle}
-                movePost={moveColumn}
-              />
-            ))}
-          </td>
-        </table>
-      );
   };
 
-const itemType="COLUMN"
-    }
-}
+  const newPosts = posts.filter(post => post.status === "new");
+  const goodPosts = posts.filter(post => post.status === "good");
+  const badPosts = posts.filter(post => post.status === "bad");
 
-export default Columns;
+  return (
+    <DndProvider backend={HTML5Backend}>
+      <div className="container">
+        <div className="row">
+          <div className="col-12">
+            <h1>Posts</h1>
+          </div>
+        </div>
+        <div className="row">
+          <ColumnArea columnName="new" posts={newPosts} moveColumn={moveColumn} />
+          <ColumnArea columnName="good" posts={goodPosts} moveColumn={moveColumn} />
+          <ColumnArea columnName="bad" posts={badPosts} moveColumn={moveColumn} />
+        </div>
+      </div>
+    </DndProvider>
+  );
+};
 
 document.addEventListener("DOMContentLoaded", () => {
-    ReactDOM.render(<Columns />, document.querySelector(".post-app"));
-  });
+  ReactDOM.render(<Posts />, document.querySelector(".post-app"));
+});
+
+const initialData = [
+  {
+    id: 0,
+    title: "Inear",
+    status: "new"
+  },
+  {
+    id: 1,
+    title: "Lovepad",
+    status: "new"
+  },
+  {
+    id: 2,
+    title: "Netplode",
+    status: "new"
+  },
+  {
+    id: 3,
+    title: "Comcur",
+    status: "new"
+  },
+  {
+    id: 4,
+    title: "Nitracyr",
+    status: "new"
+  },
+  {
+    id: 5,
+    title: "Remold",
+    status: "new"
+  },
+  {
+    id: 6,
+    title: "Namegen",
+    status: "new"
+  }
+];
+
+
