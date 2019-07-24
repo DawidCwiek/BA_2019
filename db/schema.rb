1f40193ccb36b6c2bfbd98c89aebbe4c865460fc
DawidCwiek <cwiekdawid1@gmail.com>
2019-07-24 19:48:14 +0200

    [fix] project jbilder

--- db/schema.rb
+++ db/schema.rb
@@ -20,7 +20,7 @@ ActiveRecord::Schema.define(version: 2019_07_23_121445) do
     t.integer "tasks_order", array: true
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.string "name", default: "New Column"
+    t.string "name"
     t.index ["project_id"], name: "index_columns_on_project_id"
     t.index ["tasks_order"], name: "index_columns_on_tasks_order", using: :gin
   end
@@ -29,12 +29,13 @@ ActiveRecord::Schema.define(version: 2019_07_23_121445) do
     t.string "title"
     t.text "desc"
     t.string "key"
+    t.bigint "user_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.bigint "user_id"
-    t.integer "columns_order", default: [], array: true
     t.boolean "archived", default: false
+    t.integer "columns_order", default: [], array: true
     t.index ["columns_order"], name: "index_projects_on_columns_order", using: :gin
+    t.index ["user_id"], name: "index_projects_on_user_id"
   end
 
   create_table "projects_users", force: :cascade do |t|
