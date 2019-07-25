77fc0151118f74ac272b5543c5f6173621719e8c
aroon0808 <damiangronowski23@gmail.com>
2019-07-25 12:47:22 +0200

    bug fixes

--- app/javascript/packs/users_list.jsx
+++ app/javascript/packs/users_list.jsx
@@ -1,5 +1,4 @@
 import React from "react";
-import ReactDOM from "react-dom";
 import axios from "axios";
 import ConfirmationAdmin from "./accepted_modal";
 import RemoveAdmin from "./remove_admin";
