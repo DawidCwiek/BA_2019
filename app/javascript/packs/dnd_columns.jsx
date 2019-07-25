5a50cacc7a739331b7a539dcf1348fbb1a00b7d1
timedar <timedar@gmail.com>
2019-07-25 15:12:12 +0200

    task and columns dnd

--- app/javascript/packs/dnd_columns.jsx
+++ app/javascript/packs/dnd_columns.jsx
@@ -1,72 +1,11 @@
-import React, { useState, useEffect, useRef } from "react";
+import React, { useState, useEffect } from "react";
 import ReactDOM from "react-dom";
-import { useDrag, useDrop } from "react-dnd";
 import { DndProvider } from "react-dnd";
 import HTML5Backend from "react-dnd-html5-backend";
 import { moveElement, findIndex } from "./collection_helper";
+import { ColumnArea } from "./dnd_columns_area";
 import axios from "axios";
-
-const ColumnArea = ({name, moveColumn, columns }) => {
-  const [, drop] = useDrop({
-    accept: itemType,
-    drop: (item, monitor) => {
-      if(!monitor.didDrop()) {
-        moveColumn(item.id, name);
-      }
-    }
-  });
-
-  columns.length;
-  console.log(columns)
-  columns.map(columsOrder =>  
-    
-    console.log(columsOrder.id)
-    )
-  
-  return(
-      <div className="row" ref={drop}>
-        {columns.map(column => (
-            <Column 
-            key={column.id}
-            id={column.id}
-            name={column.name}
-            moveColumn={moveColumn} />
-          ))}
-      </div>
-  );
-}
-
-const Column = ({ id, name, moveColumn }) => {
-  const ref = useRef();
-
-  const [, drag] = useDrag({
-    item: { id: id, name, type: itemType }
-  });
-
-  const [, drop] = useDrop({
-    accept: itemType,
-    drop: item => {
-      moveColumn(item.id, name, id)
-    }
-  })
-
-  drag(ref);
-  drop(ref);
-
-  return(
-  <div class="Single_Column" style={{ paddingBottom: 100 }}>
-    <div ref={ref} style={{ padding : 10 }} key={id}>
-      {name}
-    </div>
-  </div>
-  );
-};
-
-export default ColumnArea;
-
-const itemType = "COLUMN";
-/////////////////////////////////////////
-const Posts = () => {
+const Dnd = () => {
 
   const [posts, updatePost] = useState(initalColumns);
 
@@ -134,8 +73,9 @@ const Posts = () => {
 };
 
 document.addEventListener("DOMContentLoaded", () => {
-  ReactDOM.render(<Posts />, document.querySelector(".post-app"));
+  ReactDOM.render(<Dnd />, document.querySelector(".post-app"));
 });
+
 
 const initalColumns = [
   {
