4c222b8cb64c8d9a8d4e520801e98bd924a57662
timedar <timedar@gmail.com>
2019-07-26 11:35:55 +0200

    Drain column and task data from db

--- app/javascript/packs/dnd_columns_area.jsx
+++ app/javascript/packs/dnd_columns_area.jsx
@@ -1,7 +1,9 @@
-import React, { useRef, useState } from "react";
+import React, { useRef, useState, useEffect } from "react";
 import { useDrag, useDrop } from "react-dnd";
 import { TasksArea } from "./dnd_tasks";
 import { moveElement, findIndex } from "./collection_helper";
+import { black } from "ansi-colors";
+import axios from "axios";
 
 export const ColumnArea = ({name, moveColumn, columns }) => {
   const [, drop] = useDrop({
@@ -16,7 +18,7 @@ export const ColumnArea = ({name, moveColumn, columns }) => {
   const columnIds = columns.map(columsOrder => 
     columsOrder.id
     )
-  console.log(columnIds); 
+
   return(
       <div className="row" ref={drop}>
         {columns.map(column => (
@@ -47,9 +49,11 @@ const Column = ({ id, name, moveColumn }) => {
   drag(ref);
   drop(ref);
 
+
+
   const [tasks, updateTask] = useState(initalTasks);
    
-  const moveTask = (id, nameTask, targetId) => {
+  const moveTask = (id, title, targetId) => {
     updateTask(tasks => {
         console.log(id, targetId);
 
@@ -71,13 +75,44 @@ const Column = ({ id, name, moveColumn }) => {
     });
 };
 
+  useEffect(() => {
+    fetchTasks();
+  }, [])
+
+  const fetchTasks = () => {
+    axios
+        .get("/projects/1/users.json", {
+          headers: {
+            "X-CSRF-Token": document.querySelector('meta[name="csrf-token"]')
+              .content
+          }
+        })
+        .then(response => {
+          const tasks = response.data.data.task;
+          updateTask(tasks.filter(task => task.column_id === id))
+    }
+    // )
+    // axios
+    //     .patch(
+    //       `/administrators/add_admin/${this.props.user_id}`,{
+    //         headers: {
+    //           "X-CSRF-Token": document.querySelector('meta[name="csrf-token"]')
+    //             .content
+    //         }
+    //       })
+    //     .then(() => {
+    //       this.props.user_data();
+    // }
+    );
+  }
+
   return(
-  <div class="Single_Column col-4" style={{ paddingBottom: 100 }}>
+  <div className="Single_Column col-4" style={{ paddingBottom: 100 }}>
     <div ref={ref} style={{ padding : 10 }} key={id}>
-      {name}
+      <h4>{name}</h4>
     </div>
     <TasksArea
-      nameTask={tasks.nameTask}
+      title={tasks.title}
       moveTask={moveTask}
       tasks={tasks}
     />
@@ -90,30 +125,37 @@ const itemType = "COLUMN";
 const initalTasks = [
   {
     id: 0,
-    nameTask: "TassskUMN >1sza",
+    column_id: 1,
+    title: "TassskUMN >1sza",
   },
   {
     id: 1,
-    nameTask: "Tasssk",
+    column_id: 1,
+    title: "Tasssk",
   },
   {
     id: 2,
-    nameTask: "Tassske",
+    column_id: 2,
+    title: "Tassske",
   },
   {
     id: 3,
-    nameTask: "Tasssk",
+    column_id: 2,
+    title: "Tasssk",
   },
   {
     id: 4,
-    nameTask: "Tassskr",
+    column_id: 3,
+    title: "Tassskr",
   },
   {
     id: 5,
-    nameTask: "Tasssk",
+    column_id: 3,
+    title: "Tasssk",
   },
   {
     id: 6,
-    nameTask: "Tasssk",
+    column_id: 3,
+    title: "Tasssk",
   }
 ];
