a7cf1a231a0e17b9d44c1cad1a73ea743f5198cf
kucziii <kucziii@MacBook-Pro.local>
2019-07-29 15:04:57 +0200

    delete doubled method

--- app/controllers/projects/users_controller.rb
+++ app/controllers/projects/users_controller.rb
@@ -3,10 +3,4 @@ class Projects::UsersController < ApplicationController
     @project = Project.find(params[:project_id])
     render 'project/users/index'
   end
-
-  def archive_user
-    @user = User.find(params[:id])
-    @user.update(archived: true, admin: nil, active: false)
-    head :no_content
-  end
 end
