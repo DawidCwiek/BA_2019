ee5459f30219809bdc573d5b64e77870bdc87c73
kucziii <kucziii@MacBook-Pro.local>
2019-07-29 13:03:32 +0200

    [fix] delete unnecessary commented lines

--- spec/controllers/wellcome_controller_spec.rb
+++ spec/controllers/wellcome_controller_spec.rb
@@ -3,8 +3,6 @@ require 'rails_helper'
 RSpec.describe WellcomeController, type: :controller do
   describe '#index' do
     login_archived
-    # it { expect(response).to redirect_to("/wellcome") }
-    # it { expect(response).to render_template :archived }
     it 'redirects to archived' do
       get :index
       expect(response).to render_template("wellcome/archived")
