8c07875096b55f4aca545a4c4bd3482ffc34cbc6
DawidCwiek <cwiekdawid1@gmail.com>
2019-07-30 16:24:18 +0200

    permission

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -2,22 +2,25 @@ class WelcomeController < ApplicationController
   before_action :authenticate_user!
   before_action :render_layout_for_archived_user
   before_action :admin_user_redirect
+  skip_before_action :permission, only: [:index]
 
   def index; end
 
   private
 
   def admin_user_redirect
-    if current_user.admin?
-      redirect_to administrators_path
-    elsif current_user.active
-      redirect_to manage_io_index_path
+    if current_user.active && !current_user.archived
+      if current_user.admin?
+        redirect_to administrators_path
+      else
+        redirect_to manage_io_index_path
+      end
     else
       render :index
     end
   end
 
   def render_layout_for_archived_user
-    current_user.archived ? (render :archived) : (render :index)
+    render :archived, layout: 'no_logged' if current_user.archived
   end
 end
